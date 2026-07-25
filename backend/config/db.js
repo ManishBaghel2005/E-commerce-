8ee1a756909add9e6a4f88d169b5e09f6582f3f0
ManishBaghel2005 <manishedit78@gmail.com>
2026-07-25 14:57:54 +0530

    cahnge db connection add product

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const db = async()=>{
     try {
         await mongoose.connect(process.env.MONGO_URI)
-        // console.log("data connect succfully")
+        console.log("data connect succfully")
     } catch (error) {
         console.log({message:error.message})
     }
