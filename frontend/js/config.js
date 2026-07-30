a1fae640ae077dda3d12c78c52a940b9c32db1d5
ManishBaghel2005 <manishedit78@gmail.com>
2026-07-30 15:38:01 +0530

    link change backend

--- frontend/js/config.js
+++ frontend/js/config.js
@@ -1,7 +1,7 @@
 const isLocal = window.location.hostname === "localhost" || window.location.hostname === "127.0.0.1";
 
 // Render backend URL — yahi actual API server hai
-const PRODUCTION_BACKEND_URL = "https://e-commerce-yf0t.onrender.com";
+const PRODUCTION_BACKEND_URL = "https://e-commerce-gzl3.vercel.app/";
 
 const BASE_URL = isLocal
     ? "http://localhost:5000"
